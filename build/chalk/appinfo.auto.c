9d3507b75068b5bda8dfc6514ef85fc023ed31df
Alexander Kowarik <alex@kowarik.net>
2026-03-26 21:32:01 +0100

    Initial commit - minimal watch face

---
+++ build/chalk/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_process_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleProcessInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { PROCESS_INFO_CURRENT_STRUCT_VERSION_MAJOR, PROCESS_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { PROCESS_INFO_CURRENT_SDK_VERSION_MAJOR, PROCESS_INFO_CURRENT_SDK_VERSION_MINOR },
+  .process_version = { 1, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "w0001",
+  .company = "MakeAwesomeHappen",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = PROCESS_INFO_PLATFORM_CHALK,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x68, 0x24, 0xA9, 0xEA, 0x08, 0x18, 0x41, 0xDF, 0xA3, 0xFE, 0xA4, 0x74, 0x5E, 0xA8, 0xA8, 0xEC },
+  .virtual_size = 0xb6b6
+};
---
+++ src/c/w0001.c
@@ -0,0 +1,256 @@
+#include <pebble.h>
+
+#define DIAL_COLOR GColorFromRGB(249, 247, 241)
+#define BEZEL_COLOR GColorFromRGB(196, 196, 196)
+#define BEZEL_SHADOW_COLOR GColorFromRGB(228, 228, 228)
+#define MARKER_COLOR GColorFromRGB(110, 110, 110)
+#define TEXT_COLOR GColorFromRGB(92, 92, 92)
+#define ACCENT_COLOR GColorFromRGB(204, 36, 36)
+#define SECOND_HAND_COLOR GColorFromRGB(188, 180, 156)
+#define HAND_BASE_COLOR GColorFromRGB(176, 176, 176)
+
+static Window *s_window;
+static Layer *s_face_layer;
+
+static GFont s_subtitle_font;
+static GFont s_footer_font;
+static GFont s_date_font;
+
+static struct tm s_current_time;
+
+static GPoint prv_point_on_circle(GPoint center, int32_t angle, int16_t radius) {
+  return GPoint(
+      center.x + (int16_t)(sin_lookup(angle) * (int32_t)radius / TRIG_MAX_RATIO),
+      center.y - (int16_t)(cos_lookup(angle) * (int32_t)radius / TRIG_MAX_RATIO));
+}
+
+static GPoint prv_offset_point(GPoint point, int32_t angle, int16_t distance) {
+  return GPoint(
+      point.x + (int16_t)(sin_lookup(angle) * (int32_t)distance / TRIG_MAX_RATIO),
+      point.y - (int16_t)(cos_lookup(angle) * (int32_t)distance / TRIG_MAX_RATIO));
+}
+
+static void prv_draw_marker_line(GContext *ctx, GPoint center, int32_t angle,
+                                 int16_t outer_radius, int16_t inner_radius,
+                                 GColor color, uint8_t width) {
+  graphics_context_set_stroke_color(ctx, color);
+  graphics_context_set_stroke_width(ctx, width);
+  graphics_draw_line(ctx,
+                     prv_point_on_circle(center, angle, outer_radius),
+                     prv_point_on_circle(center, angle, inner_radius));
+}
+
+static void prv_draw_circle_marker(GContext *ctx, GPoint center, int32_t angle,
+                                   int16_t radius, int16_t stem_length,
+                                   uint8_t circle_radius) {
+  GPoint marker_center = prv_point_on_circle(center, angle, radius);
+  GPoint stem_end = prv_point_on_circle(center, angle, radius - stem_length);
+
+  graphics_context_set_stroke_color(ctx, MARKER_COLOR);
+  graphics_context_set_stroke_width(ctx, 1);
+  graphics_draw_line(ctx, marker_center, stem_end);
+
+  graphics_context_set_fill_color(ctx, DIAL_COLOR);
+  graphics_fill_circle(ctx, marker_center, circle_radius);
+
+  graphics_context_set_stroke_color(ctx, BEZEL_COLOR);
+  graphics_draw_circle(ctx, marker_center, circle_radius);
+}
+
+static void prv_draw_dial(GContext *ctx, GRect bounds) {
+  const GPoint center = grect_center_point(&bounds);
+  const int16_t outer_radius = bounds.size.w / 2 - 2;
+  const int16_t dial_radius = outer_radius - 4;
+  const int16_t marker_outer = dial_radius;
+  const int16_t tick_inner = marker_outer - 34;
+  const int16_t minute_tick_length = 6;
+  const int16_t minute_tick_outer = marker_outer;
+  const int16_t minute_tick_inner = minute_tick_outer - minute_tick_length;
+  const int16_t tick_stem_length = marker_outer - tick_inner;
+
+  graphics_context_set_fill_color(ctx, DIAL_COLOR);
+  graphics_fill_circle(ctx, center, dial_radius);
+
+  graphics_context_set_stroke_color(ctx, BEZEL_SHADOW_COLOR);
+  graphics_context_set_stroke_width(ctx, 4);
+  graphics_draw_circle(ctx, center, outer_radius);
+
+  graphics_context_set_stroke_color(ctx, BEZEL_COLOR);
+  graphics_context_set_stroke_width(ctx, 2);
+  graphics_draw_circle(ctx, center, dial_radius);
+
+  graphics_context_set_stroke_width(ctx, 1);
+  graphics_context_set_stroke_color(ctx, BEZEL_SHADOW_COLOR);
+  graphics_draw_circle(ctx, center, dial_radius - 3);
+
+  for (int i = 0; i < 60; ++i) {
+    if (i % 5 == 0) {
+      continue;
+    }
+    const int32_t angle = TRIG_MAX_ANGLE * i / 60;
+    prv_draw_marker_line(ctx, center, angle, minute_tick_outer, minute_tick_inner, MARKER_COLOR, 1);
+  }
+
+  for (int hour = 1; hour < 12; ++hour) {
+    if (hour == 3 || hour == 9) {
+      continue;
+    }
+
+    const int32_t angle = TRIG_MAX_ANGLE * hour / 12;
+    prv_draw_marker_line(ctx, center, angle, marker_outer, tick_inner, MARKER_COLOR, 2);
+  }
+
+  prv_draw_marker_line(ctx, center, 0, marker_outer, tick_inner, MARKER_COLOR, 2);
+  prv_draw_circle_marker(ctx, center, TRIG_MAX_ANGLE / 4, marker_outer, tick_stem_length, 4);
+  prv_draw_circle_marker(ctx, center, TRIG_MAX_ANGLE / 2, marker_outer, tick_stem_length, 4);
+  prv_draw_circle_marker(ctx, center, TRIG_MAX_ANGLE * 3 / 4, marker_outer, tick_stem_length, 4);
+
+  const int16_t top_circle_y = center.y - marker_outer;
+  const int16_t top_circle_offset = 8;
+
+  graphics_context_set_fill_color(ctx, DIAL_COLOR);
+  graphics_fill_circle(ctx, GPoint(center.x - top_circle_offset, top_circle_y), 4);
+  graphics_fill_circle(ctx, GPoint(center.x + top_circle_offset, top_circle_y), 4);
+
+  graphics_context_set_stroke_color(ctx, BEZEL_COLOR);
+  graphics_draw_circle(ctx, GPoint(center.x - top_circle_offset, top_circle_y), 4);
+  graphics_draw_circle(ctx, GPoint(center.x + top_circle_offset, top_circle_y), 4);
+}
+
+static void prv_draw_branding(GContext *ctx, GRect bounds) {
+  const GPoint center = grect_center_point(&bounds);
+
+  graphics_context_set_text_color(ctx, TEXT_COLOR);
+  graphics_draw_text(ctx, "AUTOMATIC", s_subtitle_font,
+                     GRect(center.x - 50, center.y + 2, 100, 16),
+                     GTextOverflowModeTrailingEllipsis, GTextAlignmentCenter, NULL);
+  graphics_draw_text(ctx, "MADE IN AUSTRIA", s_footer_font,
+                     GRect(center.x - 55, center.y + 72, 110, 12),
+                     GTextOverflowModeTrailingEllipsis, GTextAlignmentCenter, NULL);
+}
+
+static void prv_draw_date_window(GContext *ctx, GRect bounds) {
+  const GPoint center = grect_center_point(&bounds);
+  char date_buffer[3];
+  snprintf(date_buffer, sizeof(date_buffer), "%d", s_current_time.tm_mday);
+
+  const GRect frame = GRect(center.x + 64, center.y - 12, 34, 24);
+
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_fill_rect(ctx, frame, 0, GCornerNone);
+
+  graphics_context_set_stroke_color(ctx, ACCENT_COLOR);
+  graphics_context_set_stroke_width(ctx, 1);
+  graphics_draw_rect(ctx, frame);
+
+  graphics_context_set_text_color(ctx, ACCENT_COLOR);
+  graphics_draw_text(ctx, date_buffer, s_date_font, frame,
+                     GTextOverflowModeTrailingEllipsis, GTextAlignmentCenter, NULL);
+}
+
+static void prv_draw_hand(GContext *ctx, GPoint center, int32_t angle,
+                          int16_t length, int16_t back_length,
+                          uint8_t base_width, uint8_t highlight_width,
+                          uint8_t accent_width, int16_t accent_offset) {
+  const GPoint tip = prv_point_on_circle(center, angle, length);
+  const GPoint tail = prv_point_on_circle(center, angle + TRIG_MAX_ANGLE / 2, back_length);
+  const int32_t perpendicular = angle + TRIG_MAX_ANGLE / 4;
+
+  graphics_context_set_stroke_color(ctx, HAND_BASE_COLOR);
+  graphics_context_set_stroke_width(ctx, base_width);
+  graphics_draw_line(ctx, tail, tip);
+
+  graphics_context_set_stroke_color(ctx, GColorWhite);
+  graphics_context_set_stroke_width(ctx, highlight_width);
+  graphics_draw_line(ctx, tail, tip);
+
+  graphics_context_set_stroke_color(ctx, ACCENT_COLOR);
+  graphics_context_set_stroke_width(ctx, accent_width);
+  graphics_draw_line(ctx,
+                     prv_offset_point(center, perpendicular, accent_offset),
+                     prv_offset_point(tip, perpendicular, accent_offset));
+}
+
+static void prv_draw_hands(GContext *ctx, GRect bounds) {
+  const GPoint center = grect_center_point(&bounds);
+  const int32_t second_angle = TRIG_MAX_ANGLE * s_current_time.tm_sec / 60;
+  const int32_t minute_angle =
+      TRIG_MAX_ANGLE * (s_current_time.tm_min * 60 + s_current_time.tm_sec) / 3600;
+  const int32_t hour_angle =
+      TRIG_MAX_ANGLE *
+      (((s_current_time.tm_hour % 12) * 3600) + (s_current_time.tm_min * 60) + s_current_time.tm_sec) /
+      (12 * 3600);
+
+  prv_draw_hand(ctx, center, hour_angle, 60, 8, 8, 4, 2, 1);
+  prv_draw_hand(ctx, center, minute_angle, 88, 10, 6, 3, 1, 1);
+
+  graphics_context_set_stroke_color(ctx, SECOND_HAND_COLOR);
+  graphics_context_set_stroke_width(ctx, 1);
+  graphics_draw_line(ctx, center, prv_point_on_circle(center, second_angle, 100));
+
+  graphics_context_set_fill_color(ctx, HAND_BASE_COLOR);
+  graphics_fill_circle(ctx, center, 7);
+  graphics_context_set_fill_color(ctx, DIAL_COLOR);
+  graphics_fill_circle(ctx, center, 4);
+  graphics_context_set_stroke_color(ctx, BEZEL_COLOR);
+  graphics_draw_circle(ctx, center, 7);
+}
+
+static void prv_face_layer_update_proc(Layer *layer, GContext *ctx) {
+  const GRect bounds = layer_get_bounds(layer);
+
+  graphics_context_set_antialiased(ctx, true);
+
+  prv_draw_dial(ctx, bounds);
+  prv_draw_branding(ctx, bounds);
+  prv_draw_date_window(ctx, bounds);
+  prv_draw_hands(ctx, bounds);
+}
+
+static void prv_tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  s_current_time = *tick_time;
+  layer_mark_dirty(s_face_layer);
+}
+
+static void prv_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  const GRect bounds = layer_get_bounds(window_layer);
+
+  s_face_layer = layer_create(bounds);
+  layer_set_update_proc(s_face_layer, prv_face_layer_update_proc);
+  layer_add_child(window_layer, s_face_layer);
+}
+
+static void prv_window_unload(Window *window) {
+  layer_destroy(s_face_layer);
+}
+
+static void prv_init(void) {
+  time_t now = time(NULL);
+  s_current_time = *localtime(&now);
+
+  s_subtitle_font = fonts_get_system_font(FONT_KEY_GOTHIC_14);
+  s_footer_font = fonts_get_system_font(FONT_KEY_GOTHIC_09);
+  s_date_font = fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD);
+
+  s_window = window_create();
+  window_set_background_color(s_window, DIAL_COLOR);
+  window_set_window_handlers(s_window, (WindowHandlers) {
+    .load = prv_window_load,
+    .unload = prv_window_unload,
+  });
+
+  tick_timer_service_subscribe(SECOND_UNIT, prv_tick_handler);
+  window_stack_push(s_window, true);
+}
+
+static void prv_deinit(void) {
+  tick_timer_service_unsubscribe();
+  window_destroy(s_window);
+}
+
+int main(void) {
+  prv_init();
+  app_event_loop();
+  prv_deinit();
+}
